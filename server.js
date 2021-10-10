75806537d12892828493dfbfe24b652815f63466
Nakul Sharma <56245793+nakulsharma-tal@users.noreply.github.com>
2021-10-10 19:45:21 +0530

    added create user API

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 // server.js
 // where your node app starts
 
-const express = require("express");
-const rTracer = require("cls-rtracer");
-const { ApiLoggerMiddleware, Logger } = require("./logger");
-const cors = require("cors");
-const { urlencoded } = require("body-parser");
-const { connectToDb } = require("./db/utils/connect.util");
+const express = require('express');
+const rTracer = require('cls-rtracer');
+const { ApiLoggerMiddleware, Logger } = require('./logger');
+const cors = require('cors');
+const { urlencoded } = require('body-parser');
+const { connectToDb } = require('./db/utils/connect.util');
+const { User } = require('./db/models/user.model');
 
-require("dotenv").config();
+require('dotenv').config();
 
 // init project
 var app = express();
@@ -27,22 +28,43 @@ app.use(rTracer.expressMiddleware(), ApiLoggerMiddleware);
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/views/index.html");
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 // your first API endpoint...
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+app.post('/api/users', function (req, res) {
+  const username = req.body.username;
+  Logger.info('Server.Post.Users.started', {
+    username,
+  });
+
+  User.create({
+    username,
+  })
+    .then((newUser) => {
+      Logger.info('Server.Post.Users.success', {
+        newUser,
+      });
+      res.json({
+        username: newUser.username,
+        _id: newUser.id,
+      });
+    })
+    .catch((error) => {
+      Logger.error('Server.Post.Users.failed', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    });
 });
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  Logger.info(
-    "Server",
-    `Your app is listening on port ${listener.address().port}`
-  );
+  Logger.info('Server', `Your app is listening on port ${listener.address().port}`);
 });
